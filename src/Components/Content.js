69e920996020d892f858232db29d1dff5704ef64
FredrikStrandseter <fredrikbss@gmail.com>
2021-11-05 11:53:29 +0100

    shit ass

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -4,6 +4,7 @@ function Content() {
   return (
     <div className="bg-gray-50">
       <div className="flex justify-center">
+        <p>shit ass</p>
         <img
           className="mt-20 rounded-2xl"
           src="https://dreamsmean.net/images/media/213551-dream-meaning-about-a-clothing-store-1.jpg"
