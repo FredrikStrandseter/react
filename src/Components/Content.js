06a0cbd484fc0de6e1861a888772b2e2fc84c97a
FredrikStrandseter <fredrikbss@gmail.com>
2021-11-09 13:39:42 +0100

    14:00, 09.11.2021
    WOW

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -4,13 +4,15 @@ function Content() {
   return (
     <div className="bg-gray-50">
       <div className="flex justify-center">
-        <p>shit ass</p>
         <img
-          className="mt-20 rounded-2xl"
+          className="mt-16 rounded-2xl"
           src="https://dreamsmean.net/images/media/213551-dream-meaning-about-a-clothing-store-1.jpg"
           width="1200"
           alt=""
         />
+      </div>
+      <div className="pl-40 pt-20 text-3xl text-transform: capitalize">
+        <p>categories</p>
       </div>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,10 @@ import { AiOutlineUser } from "react-icons/ai";
 
 function Header() {
   return (
-    <div className="bg-black h-20">
+    <div className="bg-black h-16">
       <div className="items-center">
-        <img src="/netshoplogo.png" alt="" className="absolute left-1 w-40" />
-        <AiOutlineUser className="text-white absolute right-1 m-7" />
+        <img src="/netshoplogo.png" alt="" className="absolute left-1 w-32" />
+        <AiOutlineUser className="text-white absolute right-2 m-4 text-4xl" />
       </div>
     </div>
   );
