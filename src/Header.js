dad512938703ca404b098f0a935106dac2659313
FredrikStrandseter <fredrikbss@gmail.com>
2021-11-10 08:53:27 +0100

    Tirsdag

--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,9 @@ function Header() {
         <img src="/netshoplogo.png" alt="" className="absolute left-1 w-32" />
         <AiOutlineUser className="text-white absolute right-2 m-4 text-4xl" />
       </div>
+      <div className="ml-96 justify-items-center box-border h-10 w-96 border-2 border-gray-300 rounded-lg text-gray-300">
+        yo yo yo
+      </div>
     </div>
   );
 }
