7b4c91630daff691e152d0eb5d1aec40be5f825b
FredrikStrandseter <fredrikbss@gmail.com>
2021-11-12 15:26:02 +0100

    Fredag

---
+++ src/Components/Shop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <p>yo</p>
+    </div>
+  );
+};
+
+export default Shop;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { AiOutlineUser, AiOutlineSearch } from "react-icons/ai";
-
+/* import { Component } from "react"; */
+/* import Content from "./Components/Content";
+import Shop from "./Components/Shop";
+import liked from "./Components/liked";
+ */
 const Header = () => {
   return (
     <>
       {/* Top header */}
       <div className="fixed top-0 z-50 overflow-hidden w-full">
-        <div className="bg-gray-800 h-16 flex justify-center">
-          <div className="flex items-center h-full w-4/5 justify-between">
-            <div className="">
-              <img className="w-32" src="/Hvitnetshop.png" alt="" />
-            </div>
+        <div className="bg-gray-800 h-16">
+          <div className="flex items-center h-full w-4/5 mx-auto justify-between">
+            <img className="w-32" src="/Hvitnetshop.png" alt="" />
             {/* Søkefelt */}
             <div className="relative flex items-center">
               <input
@@ -27,10 +29,25 @@ const Header = () => {
         </div>
         {/* Bottom header */}
         <div className="bg-gray-900 opacity-80 h-11 flex">
-          <div className="flex justify-around w-96 text-white self-center ml-36 pl-1 font-sans">
-            <a href="/">Home</a>
-            <a href="/Shop">Shop</a>
-            <a href="/Liked">Liked</a>
+          <div className="flex mx-auto w-4/5 text-white self-center space-x-20">
+            <a
+              className="transition transform duration-300 ease-out hover:scale-110"
+              href="/content"
+            >
+              Home
+            </a>
+            <a
+              className="transition transform duration-300 ease-out hover:scale-110"
+              href="/Shop"
+            >
+              Shop
+            </a>
+            <a
+              className="transition transform duration-300 ease-out hover:scale-110"
+              href="/Liked"
+            >
+              Liked
+            </a>
           </div>
         </div>
       </div>
