2cd53a1af40adebb5473e357c23c4d6d208075e2
FredrikStrandseter <fredrikbss@gmail.com>
2021-11-23 19:24:29 +0100

    Tirsdag hjemme

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -6,15 +6,15 @@ function Home() {
       <div className=" w-4/5 mx-auto mt-20">
         <div className="flex justify-center">
           <img
-            className="mt-16 rounded-2xl m-4/5"
-            src="https://dreamsmean.net/images/media/213551-dream-meaning-about-a-clothing-store-1.jpg"
+            className="mt-16 rounded-2xl"
+            src="framside net.shop.jpg"
             alt=""
           />
         </div>
         <div className="mt-20 text-transform: capitalize mb-5">
           <p className="text-5xl">Categories</p>
         </div>
-        <div className="flex justify-around">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
           <img
             className="mt-8 rounded-md mb-10"
             width={300}
